60e7fd324d91712153662c50f86a12af9dbaebe7
JP <jp@johnprinz.com>
2026-03-12 14:27:11 -0400

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <nav className='navbar'>
+                <h2>My App</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
