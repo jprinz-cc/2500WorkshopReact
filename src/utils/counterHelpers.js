f3b7985d71aaf6a17543d4cbdff270aae4f4793c
JP <jp@johnprinz.com>
2026-06-15 18:25:35 -0400

    Modified counterHelper;

--- src/utils/counterHelpers.js
+++ src/utils/counterHelpers.js
@@ -1,5 +1,5 @@
 export const formatCountDisplay = (count) => {
     if (count < 0) return "Count: 0";
-    if (count > 99) return "Count: 99+";
+    if (count > 999) return "Count: 999+";
     return `Count: ${count}`;
 };
